64298fa4087416e98bc9ef0556e310531c1d1fb0
Kyle Meyer <kyle@kyleam.com>
2019-04-02 14:35:05 -0400

    dir: do not traverse repositories with no commits
    When treat_directory() encounters a directory that is not in the index
    and DIR_NO_GITLINKS is unset, it calls resolve_gitlink_ref() to decide
    if a directory looks like a repository, in which case the directory
    won't be traversed.  As a result, 'status -uall' and 'ls-files -o'
    will show only the directory, even when there are untracked files
    within the directory.
    
    For the unusual case where a repository doesn't have any commits,
    resolve_gitlink_ref() returns -1 because HEAD cannot be resolved, and
    the directory is treated as a normal directory (i.e. traversal does
    not stop at the repository boundary).  The status and ls-files
    commands above list untracked files within the repository rather than
    showing only the top-level directory.
    
    The above case is a corner case in an already unusual situation of the
    working tree containing a repository that is not a tracked submodule,
    but we might as well treat anything that looks like a repository
    consistently.  Loosen the "looks like a repository" criteria in
    treat_directory() by replacing resolve_gitlink_ref() with
    is_nonbare_repository_dir(), one of the checks that is performed
    downstream when resolve_gitlink_ref() is called with an empty
    repository.
    
    As the required update to t3700-add shows, being looser with the check
    means that we're stricter when adding empty repositories to the index:
    
      % git add repo
      warning: adding embedded git repository: repo
      hint: You've added another git repository inside your current repository.
      hint: [...]
      error: unable to index file 'repo/'
      fatal: adding files failed
    
    That error message isn't particularly helpful in this situation, but
    it seems preferable to the old behavior of adding the repository's
    untracked files.  And if the caller really wants the previous
    behavior, they can get it by adding a trailing slash.
    
    Signed-off-by: Kyle Meyer <kyle@kyleam.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3009-ls-files-others-nonsubmodule.sh
+++ t/t3009-ls-files-others-nonsubmodule.sh
@@ -8,6 +8,14 @@ This test runs git ls-files --others with the following working tree:
       directory with no files aside from a bogus .git file
     repo-bogus-untracked-file/
       directory with a bogus .git file and another untracked file
+    repo-no-commit-no-files/
+      git repository without a commit or a file
+    repo-no-commit-untracked-file/
+      git repository without a commit but with an untracked file
+    repo-with-commit-no-files/
+      git repository with a commit and no untracked files
+    repo-with-commit-untracked-file/
+      git repository with a commit and an untracked file
 '
 
 . ./test-lib.sh
@@ -17,6 +25,10 @@ test_expect_success 'setup: expected output' '
 	expected
 	output
 	repo-bogus-untracked-file/untracked
+	repo-no-commit-no-files/
+	repo-no-commit-untracked-file/
+	repo-with-commit-no-files/
+	repo-with-commit-untracked-file/
 	EOF
 '
 
@@ -25,7 +37,15 @@ test_expect_success 'setup: directories' '
 	echo foo >repo-bogus-no-files/.git &&
 	mkdir repo-bogus-untracked-file &&
 	echo foo >repo-bogus-untracked-file/.git &&
-	: >repo-bogus-untracked-file/untracked
+	: >repo-bogus-untracked-file/untracked &&
+	git init repo-no-commit-no-files &&
+	git init repo-no-commit-untracked-file &&
+	: >repo-no-commit-untracked-file/untracked &&
+	git init repo-with-commit-no-files &&
+	git -C repo-with-commit-no-files commit --allow-empty -mmsg &&
+	git init repo-with-commit-untracked-file &&
+	test_commit -C repo-with-commit-untracked-file msg &&
+	: >repo-with-commit-untracked-file/untracked
 '
 
 test_expect_success 'ls-files --others handles non-submodule .git' '
