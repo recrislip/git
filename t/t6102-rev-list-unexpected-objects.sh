83199d0c72588eeec0b0ff8d6fb795a9f4407278
Jeff King <peff@peff.net>
2019-04-04 20:37:54 -0700

    rev-list: let traversal die when --missing is not in use
    Commit 7c0fe330d5 (rev-list: handle missing tree objects properly,
    2018-10-05) taught the traversal machinery used by git-rev-list to
    ignore missing trees, so that rev-list could handle them itself.
    
    However, it does so only by checking via oid_object_info_extended() that
    the object exists at all. This can miss several classes of errors that
    were previously detected by rev-list:
    
      - type mismatches (e.g., we expected a tree but got a blob)
    
      - failure to read the object data (e.g., due to bitrot on disk)
    
    This is especially important because we use "rev-list --objects" as our
    connectivity check to admit new objects to the repository, and it will
    now miss these cases (though the bitrot one is less important here,
    because we'd typically have just hashed and stored the object).
    
    There are a few options to fix this:
    
     1. we could check these properties in rev-list when we do the existence
        check. This is probably too expensive in practice (perhaps even for
        a type check, but definitely for checking the whole content again,
        which implies loading each object into memory twice).
    
     2. teach the traversal machinery to differentiate between a missing
        object, and one that could not be loaded as expected. This probably
        wouldn't be too hard to detect type mismatches, but detecting bitrot
        versus a truly missing object would require deep changes to the
        object-loading code.
    
     3. have the traversal machinery communicate the failure to the caller,
        so that it can decide how to proceed without re-evaluting the object
        itself.
    
    Of those, I think (3) is probably the best path forward. However, this
    patch does none of them. In the name of expediently fixing the
    regression to a normal "rev-list --objects" that we use for connectivity
    checks, this simply restores the pre-7c0fe330d5 behavior of having the
    traversal die as soon as it fails to load a tree (when --missing is set
    to MA_ERROR, which is the default).
    
    Note that we can't get rid of the object-existence check in
    finish_object(), because this also handles blobs (which are not
    otherwise checked at all by the traversal code).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t6102-rev-list-unexpected-objects.sh
+++ t/t6102-rev-list-unexpected-objects.sh
@@ -29,7 +29,7 @@ test_expect_success 'setup unexpected non-tree entry' '
 	broken_tree="$(git hash-object -w --literally -t tree broken-tree)"
 '
 
-test_expect_failure 'traverse unexpected non-tree entry (lone)' '
+test_expect_success 'traverse unexpected non-tree entry (lone)' '
 	test_must_fail git rev-list --objects $broken_tree
 '
 
@@ -64,7 +64,7 @@ test_expect_success 'setup unexpected non-tree root' '
 		broken-commit)"
 '
 
-test_expect_failure 'traverse unexpected non-tree root (lone)' '
+test_expect_success 'traverse unexpected non-tree root (lone)' '
 	test_must_fail git rev-list --objects $broken_commit
 '
 
